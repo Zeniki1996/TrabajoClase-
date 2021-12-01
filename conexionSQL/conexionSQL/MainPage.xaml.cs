b8216bd7f495de41c5cd53cdf0dc3df35d249cee
Nikita Martínez <62626494+Zeniki1996@users.noreply.github.com>
2021-12-01 10:41:21 -0500

    Add files via upload

---
+++ conexionSQL/conexionSQL/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Data.SqlClient;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace conexionSQL
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //const string ConnectionString = @"SERVER=IdeaPad\SQLEXPRESS;Initial Catalog=NORTHWIND;Integrated Security=SSPI";
+        private string connectionString =
+        @"Data Source=IDEAPAD\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=SSPI";
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection sqlConn = new SqlConnection(connectionString))
+            {
+                sqlConn.Open();
+                label1.Text = sqlConn.State.ToString();
+            }
+        }
+    }
+}
